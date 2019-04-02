29f716ef9827a7588cf680e7bbf63e5c7d5efa37
tobzville <tobzville@yahoo.com>
2019-04-02 15:18:12 +0100

    updated database...

--- venv/dbproject.sql
+++ venv/dbproject.sql
@@ -30,44 +30,44 @@ CREATE TABLE products (
 
 	INSERT INTO `products`
     VALUES (2,'Men Watch',29.99,'Elegant Men Wrist watch','wristwatch1.jpg',4,5),
-    (3,'Leather Watches',15.00,'Water proof leather wrist watch','wristwatch2.jpg',3,5),
-    (4,'Bracelet Watches',10.00,'Beautiful ladies hand braclet wrist watch','wristwatch3.jpg',3,5),
+    (3,'New Laptop',550.80,'Hp 3-1 function','laptop1.jpg',4,3),
+    (4,'Bracelet Watches',10.00,'Beautiful ladies hand braclet wrist watch','wristwatch2.jpg',3,5),
     (5,'Sliver Watches',20.50,'Sliver coated wrist watch for all gender','wristwatch4.jpg',2,5),
-    (6,'Unique watch',9.50,'Suitable for everyone','wristwatch5.jpg',5,5),
-    (7,'Kada bracelet',35.00,'Shop hand kada bracelet with ring','bracelet1.jpg',4,5),
+    (6,'Iphone 7',1000,'Apple iPhone 7','laptop6.jpg',14,3),
+    (7,'Men Knittle',35.00,'Cute Men Knittle Jacket','men6.jpg',5,1),
     (8,'Sliver Bracelet',20.00,'Hand sterling Sliver bracelet','bracelet2.jpg',3,5),
-    (9,'Gold Bracelet',20.59,'Gold plated hand bracelet','bracelet3.jpg',7,5),
-    (10,'Gold hand chain',29.50,'Fashion bracelet gold hand chain','bracelet4.jpg',5,5),
-    (11,'Necklace',73.38,'Interlocking sliver necklace and bracelet','bracelet5.jpg',4,5),
+    (9,'Men dress shoe',60,'Black men dress shoe','men8.jpg',8,1),
+    (10,'Scooby-Doo',20.80,'Kids halloween movie','movie2.jpg',12,4),
+    (11,'Super Heros',50,'Lego Super heroes 2','movie4.jpg',14,4),
     (12,'Necklace',12.99,'Multi strand necklace','bracelet6.jpg',14,5),
-    (13,'Mini necklace',73.38,'Beautiful, attractive mini necklace','bracelet7.jpg',2,5),
-    (14,'Men shirt',50.00,'Slim fit men long sleeve shirt','men1.jpg',15,1),
-    (15,'Men suit',100.85 ,'fitted men suit', 'men2.jpg',10,1),
+    (13,'Short dress',100,'Ladies midi dress','women2.jpg',24,6),
+    (14,'Shoulder bag',40.80,'Women comfort Shoulder bag','women5.jpg',12,6),
+    (15,'Ladies high heels',150.80,'Available in various colors','women8.jpg',12,6),
     (16,'Men T-shirt',20,'Men fitted shirt','men3.jpg',20,1),
     (17,'Men blazer',30,'Men fitted blazer','men4.jpg',30,1),
     (18,'Men Jacket',24.56,'Men Brown Jacket','men5.jpg',14,1),
-    (19,'Men Knittle',35.00,'Cute Men Knittle Jacket','men6.jpg',5,1),
+    (19,'Kada bracelet',35.00,'Shop hand kada bracelet with ring','bracelet1.jpg',4,5),
     (20,'Hugh loafers',50,'Hugh perforated penny loafers','men7.jpg',11,1),
-    (21,'Men dress shoe',60,'Black men dress shoe','men8.jpg',8,1),
-    (22,'New Laptop',550.80,'Hp 3-1 function','laptop1.jpg',4,3),
+    (21,'Gold Bracelet',20.59,'Gold plated hand bracelet','bracelet3.jpg',7,5),
+    (22,'Bracelet Watches',10.00,'Beautiful ladies hand braclet wrist watch','wristwatch3.jpg',3,5),
     (23,'Hp Essential',1000,'Hp Essential personal laptop','laptop2.jpg',4,3),
 	(24,'New Mac',2550.80,'Mac for hackering','laptop3.jpg',2,3),
-    (25,'Iphone 7',1000,'Apple iPhone 7','laptop6.jpg',14,3),
+    (25,'Unique watch',9.50,'Suitable for everyone','wristwatch5.jpg',5,5),
 	(26,'Samsung Phone',550.80,'Samsung galaxy S5 Black','laptop5.jpg',12,3),
     (27,'New Dell',1500,'Dell inspiron personal laptop (i3542-)','laptop4.jpg',4,3),
     (28,'Ladies dress',150.40,'Summer women midi dress','women1.jpg',14,6),
-    (29,'Short dress',100,'Ladies midi dress','women2.jpg',24,6),
+    (29,'Mini necklace',73.38,'Beautiful, attractive mini necklace','bracelet7.jpg',2,5),
 	(30,'Long sleeve dress',50.80,'Women button down long sleeve','women3.jpg',12,6),
     (31,'Leather Bag',50,'Women Pu leather dollar bag','women4.jpg',14,6),
-	(32,'Shoulder bag',40.80,'Women comfort Shoulder bag','women5.jpg',12,6),
+	(32,'Men shirt',50.00,'Slim fit men long sleeve shirt','men1.jpg',15,1),
     (33,'Laptop handbag',30,'Comfortable to use','women6.jpg',4,6),
     (34,'Ladies loafers',50,'Flat durable loafers','women7.jpg',14,6),
-	(35,'Ladies high heels',150.80,'Available in various colors','women8.jpg',12,6),
+	(35,'Men suit',100.85 ,'fitted men suit', 'men2.jpg',10,1),
     (36,'Ladies office shoe',25,'cute with different colors','women9.jpg',4,6),
     (37,'Venom',20,'Venom by tom hardy','movie1.jpg',14,4),
-	(38,'Scooby-Doo',20.80,'Kids halloween movie','movie2.jpg',12,4),
+	(38,'Gold hand chain',29.50,'Fashion bracelet gold hand chain','bracelet4.jpg',5,5),
     (39,'Avengers',30,'Avengers : infinity war','movie3.jpg',4,4),
-    (40,'Super Heros',50,'Lego Super heroes 2','movie4.jpg',14,4),
+    (40,'Necklace',73.38,'Interlocking sliver necklace and bracelet','bracelet5.jpg',4,5),
 	(41,'Switch game',53.80,'Nintendo switch game','movie5.jpg',12,4),
     (42,'Video games',25,'video games','movie6.jpg',4,4);
 
@@ -156,3 +156,29 @@ IF new.stock = 0 THEN
 END IF;
 END $$
 DELIMITER ;
+
+
+drop table if exists purchase_history;
+CREATE TABLE purchase_history (
+p_historyID int(11) NOT NULL auto_increment primary KEY,
+userId int,
+p_name text,
+quantity int,
+productId int(11) NOT NULL,
+FOREIGN KEY (userId) REFERENCES users(userId),
+FOREIGN KEY (productId) REFERENCES products(productId));
+
+INSERT INTO purchase_history
+VALUES (null, 3, 'Gold Bracelet', 3, 9), (null, 9, 'New Laptop', 1, 22);
+
+
+DROP TABLES IF EXISTS bestselling_products;
+CREATE TABLE bestselling_products(
+bestsellingID int(11) NOT NULL auto_increment primary key,
+productId int(11) NOT NULL,
+p_name text,
+quantity int,
+FOREIGN KEY (productId) REFERENCES products(productId));
+
+INSERT INTO bestselling_products
+VALUES (null, 17, 'Men blazer', 20);
